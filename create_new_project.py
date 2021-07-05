596e452b56908f1a24ce731c520b4529d75eb4b1
Ann Kennedy <ann.kennedy@northwestern.edu>
2021-07-05 18:05:16 -0500

    simplifying Google Drive download

--- create_new_project.py
+++ create_new_project.py
@@ -4,34 +4,12 @@ import argparse
 from shutil import copytree
 import requests
 import zipfile
+import gdown
 
 
 def download_from_google_drive(id, destination):
-    URL = "https://drive.google.com/uc?export=download"
-
-    session = requests.Session()
-    response = session.get(URL, params={'id': id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {'id': id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
-    save_response_content(response, destination)
-
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-    return None
-
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
+    URL = "https://drive.google.com/uc?id=" + id
+    gdown.download(URL, destination, quiet=False)
 
 
 def create_new_project(location, name, download_MARS_checkpoints=True, download_demo_data=False):
