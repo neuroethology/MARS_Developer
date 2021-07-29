c1b2476e9886fa99c886c5fdd973d7167e646f8f
Ann Kennedy <ann.kennedy@northwestern.edu>
2021-07-29 15:27:53 -0500

    adding export code

--- hourglass_pose/export_pose.py
+++ hourglass_pose/export_pose.py
@@ -5,13 +5,15 @@ from __future__ import print_function
 import argparse
 import os
 import sys
+import yaml
 
 import tensorflow as tf
 from tensorflow.python.framework import dtypes
-from tensorflow.python.framework import importer
-from tensorflow.python.platform import gfile
+# from tensorflow.python.framework import importer
+# from tensorflow.python.platform import gfile
 from tensorflow.python.framework import graph_util
 from tensorflow.python.tools import optimize_for_inference_lib
+from tensorflow.python.util import deprecation
 
 slim = tf.contrib.slim
 import pdb
@@ -19,29 +21,29 @@ import pickle
 import numpy as np
 
 sys.path.insert(0, os.path.abspath('..'))
-import multibox_detection.model_detection as model_detection
+import model_pose
+from config import parse_config_file
 
-def export(checkpoint_path, export_dir, export_version, prior_path, strain, view):
+deprecation._PRINT_DEPRECATION_WARNINGS = False
+
+
+def do_export(checkpoint_path, export_dir, model_name, num_parts, num_stacks):
+
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.DEBUG)
 
     graph = tf.Graph()
 
     input_node_name = "images"
-    output_node_name = None
+    output_node_name = "output_heatmaps"
 
     with graph.as_default():
 
-        input_height = 299
-        input_width = 299
+        input_height = 256
+        input_width = 256
         input_depth = 3
 
-        #we assume that we have already preprocessed the image
-        input_placeholder = tf.placeholder(tf.float32, [None, input_height * input_width * input_depth], name=input_node_name)
-        images = tf.reshape( input_placeholder, [-1, input_height,input_width,input_depth])
-
-        with open(prior_path,'rb') as f:
-            priors_bbox  =pickle.load(f, encoding='latin1')
-        priors_bbox = np.array(priors_bbox).astype(np.float32)
-
+        #we assume that we have already preprocessed the image bboxes and bboxes
+        images_bboxes = tf.compat.v1.placeholder(tf.float32,[None, input_height, input_width, input_depth], name=input_node_name)
 
         #build model detection
         batch_norm_params = {
@@ -49,7 +51,7 @@ def export(checkpoint_path, export_dir, export_version, prior_path, strain, view
             'decay': 0.9997,
             # epsilon to prevent 0s in variance.
             'epsilon': 0.001,
-            'variables_collections': [tf.GraphKeys.MOVING_AVERAGE_VARIABLES],
+            'variables_collections': [tf.compat.v1.GraphKeys.MOVING_AVERAGE_VARIABLES],
             'is_training': False
         }
         with slim.arg_scope([slim.conv2d],
@@ -57,21 +59,21 @@ def export(checkpoint_path, export_dir, export_version, prior_path, strain, view
                             normalizer_fn=slim.batch_norm,
                             normalizer_params=batch_norm_params,
                             weights_regularizer=slim.l2_regularizer(0.00004),
-                            biases_regularizer=slim.l2_regularizer(0.00004)):
+                            biases_regularizer=slim.l2_regularizer(0.00004)) as scope:
 
-            locations, confidences, inception_vars = model_detection.build(
-                inputs=images,
-                num_bboxes_per_cell=11,
-                reuse=False,
-                scope=''
+            predicted_heatmaps = model_pose.build(
+                input = images_bboxes,
+                num_parts = num_parts,
+                num_stacks = num_stacks
             )
-
-            priors_bboxes = tf.constant(priors_bbox, name='priors_bboxes')
-            predicted_loc = tf.add(locations,priors_bboxes,name='predicted_locations')
-
+            output_node = tf.identity(predicted_heatmaps[-1], output_node_name)
 
         variable_averages = tf.train.ExponentialMovingAverage(0.9999)
-        variables_to_restore = variable_averages.variables_to_restore(slim.get_model_variables())
+        # variables_to_restore = variable_averages.variables_to_restore(slim.get_model_variables())
+        variables_to_restore = {
+            variable_averages.average_name(var): var
+            for var in slim.get_model_variables()
+            }
 
         # retrieve checkpoint
         if os.path.isdir(checkpoint_path):
@@ -90,53 +92,69 @@ def export(checkpoint_path, export_dir, export_version, prior_path, strain, view
         input_graph_def = graph.as_graph_def()  # graph used to retrieve the nodes
 
         #configure the session
-        sess_config = tf.ConfigProto(
+        sess_config = tf.compat.v1.ConfigProto(
                 log_device_placement= False,
                 allow_soft_placement = True,
-                gpu_options = tf.GPUOptions(
+                gpu_options = tf.compat.v1.GPUOptions(
                     per_process_gpu_memory_fraction=0.9
                 )
             )
-        sess = tf.Session(graph=graph, config=sess_config)
+        sess = tf.compat.v1.Session(graph=graph, config=sess_config)
 
         #start the session and restore the graph weights
         with sess.as_default():
 
-            tf.global_variables_initializer().run()
+            tf.compat.v1.global_variables_initializer().run()
 
             saver.restore(sess, checkpoint_path)
-
             #export varibales to constants
             constant_graph_def = graph_util.convert_variables_to_constants(
                 sess=sess,
                 input_graph_def=input_graph_def,
-                output_node_names=[predicted_loc.name[:-2],confidences.name[:-2]])
+                output_node_names=[output_node.name[:-2]])
 
             optimized_graph_def = optimize_for_inference_lib.optimize_for_inference(
                 input_graph_def=constant_graph_def,
                 input_node_names=[input_node_name],
-                output_node_names=[predicted_loc.name[:-2],confidences.name[:-2]],
+                output_node_names=[output_node.name[:-2]],
                 placeholder_type_enum=dtypes.float32.as_datatype_enum)
 
-            # serialize and dump the putput graph to fs
+            # serialize and dump the output graph to fs
             if not os.path.exists(export_dir):
                 os.makedirs(export_dir)
-            save_path = os.path.join(export_dir, 'optimized_model_%s_detection_%s_%d.pb' % (view,strain,export_version,))
-            with tf.gfile.GFile(save_path, 'wb') as f:
+            save_path = os.path.join(export_dir, model_name)
+            with tf.io.gfile.GFile(save_path, 'wb') as f:
                 f.write(optimized_graph_def.SerializeToString())
 
             print("Saved optimized model for mobile devices at: %s." % (save_path,))
             print("Input node name: %s" % (input_node_name,))
-            print("Output node name: %s %s" % (predicted_loc.name[:-2],confidences.name[:-2]))
+            print("Output node name: %s" % (output_node.name[:-2],))
             print("%d ops in the final graph." % len(optimized_graph_def.node))
 
 
+def export(project, pose_model_names=None):
+    # load project config and model config
+    config_fid = os.path.join(project, 'project_config.yaml')
+    with open(config_fid) as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    mdl_cfg = parse_config_file(os.path.join(project, 'pose', 'config_train.yaml'))
 
+    export_dir = project
+    num_parts = len(cfg['keypoints'])
+    num_stacks = mdl_cfg.NUM_STACKS
+    if not pose_model_names:
+        pose_model_list = cfg['pose']
+        pose_model_names = pose_model_list.keys()
+
+    for model in pose_model_names:
+        checkpoint_path = os.path.join(project, 'pose', model + '_model')
+        model_name = cfg['project_name'] + '_' + model + '_pose.pb'
+        do_export(checkpoint_path, export_dir, model_name, num_parts, num_stacks)
 
 
 def parse_args():
 
-    parser = argparse.ArgumentParser(description='Test an export network')
+    parser = argparse.ArgumentParser(description='Test an Hourglass export network')
 
     parser.add_argument('--checkpoint_path', dest='checkpoint_path',
                           help='Path to the specific model you want to export.',
@@ -150,18 +168,17 @@ def parse_args():
                         help='Version number of the model.',
                         required=True, type=int)
 
-    parser.add_argument('--prior_path', dest='prior_path',
-                        help='Path to prior bboxes.',
-                        required=True, type=str)
-
-    parser.add_argument('--strain', dest='strain',
-                        help='Mouse color.',
-                        required=True, type=str)
-
     parser.add_argument('--view', dest='view',
-                        help='Top or Front.',
+                        help='Top or front.',
                         required=True, type=str)
 
+    parser.add_argument('--num_parts', dest='num_parts',
+                        help='Number of parts.',
+                        required=True, type=int)
+
+    parser.add_argument('--num_stacks', dest='num_stacks',
+                        help='Number of stacks in the model.',
+                        required=True, type=int)
 
     args = parser.parse_args()
 
@@ -171,4 +188,5 @@ if __name__ == '__main__':
 
     args = parse_args()
 
-    export(args.checkpoint_path, args.export_dir, args.export_version, args.prior_path,args.strain, args.view)
+    export(args.checkpoint_path, args.export_dir, args.export_version, args.view, args.num_parts, args.num_stacks)
+
